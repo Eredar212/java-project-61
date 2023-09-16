1a5a3054fa6af9803e62cce64d604a074f702166
Виктор <viktor@MacBook-Air-test.local>
2023-09-16 15:06:40 +0300

    changed welcome stage

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,14 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void welcome() {
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+        Scanner scanner = new Scanner(System.in);
+        String playerName = scanner.next();
+        System.out.println("Hello, " + playerName + "!");
+        scanner.close();
+    }
+}
